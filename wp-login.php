52379fe445603b724dedf603bdffe36040abe271
miqdadrihdo <125847784+miqdadrihdo@users.noreply.github.com>
2024-02-05 17:11:09 +0700

    mycv

---
+++ wp-login.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="id">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Log Masuk &lsaquo; M. Miqdad Ridho. U &#8212; WordPress</title>
+	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link rel="stylesheet" id="dashicons-css" href="https://miqdadridho.github.io/mycv/wp-includes/css/dashicons.min.css?ver=6.4.3" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://miqdadridho.github.io/mycv/wp-includes/css/buttons.min.css?ver=6.4.3" media="all">
+<link rel="stylesheet" id="forms-css" href="https://miqdadridho.github.io/mycv/wp-admin/css/forms.min.css?ver=6.4.3" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://miqdadridho.github.io/mycv/wp-admin/css/l10n.min.css?ver=6.4.3" media="all">
+<link rel="stylesheet" id="login-css" href="https://miqdadridho.github.io/mycv/wp-admin/css/login.min.css?ver=6.4.3" media="all">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+	<link rel="icon" href="https://miqdadridho.github.io/mycv/wp-content/uploads/2024/02/MMRU-150x150.png" sizes="32x32">
+<link rel="icon" href="https://miqdadridho.github.io/mycv/wp-content/uploads/2024/02/MMRU-300x300.png" sizes="192x192">
+<link rel="apple-touch-icon" href="https://miqdadridho.github.io/mycv/wp-content/uploads/2024/02/MMRU-300x300.png">
+<meta name="msapplication-TileImage" content="https://miqdadridho.github.io/mycv/wp-content/uploads/2024/02/MMRU-300x300.png">
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-id-id">
+	<script>document.body.className = document.body.className.replace('no-js','js');</script>
+
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Dipersembahkan oleh WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="http://localhost/mycv/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Nama Pengguna atau Alamat Email</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username" required="required">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Sandi</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false" required="required">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Tampilkan sandi">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Ingat Saya</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log Masuk">
+									<input type="hidden" name="redirect_to" value="http://localhost/mycv/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+				<a class="wp-login-lost-password" href="https://miqdadridho.github.io/mycv/wp-login.php?action=lostpassword">Lupa sandi Anda?</a>			</p>
+			<script>function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
+		<p id="backtoblog">
+			<a href="https://miqdadridho.github.io/mycv/">&larr; Pergi ke M. Miqdad Ridho. U</a>		</p>
+			</div>
+				<div class="language-switcher">
+				<form id="language-switcher" action="" method="get">
+
+					<label for="language-switcher-locales">
+						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
+						<span class="screen-reader-text">
+							Bahasa						</span>
+					</label>
+
+					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
+<option value="id_ID" lang="id" selected data-installed="1">Bahasa Indonesia</option></select>
+					
+					
+					
+						<input type="submit" class="button" value="Ubah">
+
+					</form>
+				</div>
+				<script src="https://miqdadridho.github.io/mycv/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js"></script>
+<script src="https://miqdadridho.github.io/mycv/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1" id="jquery-migrate-js"></script>
+<script id="zxcvbn-async-js-extra">var _zxcvbnSettings = {"src":"https:\/\/miqdadridho.github.io\/mycv\/wp-includes\/js\/zxcvbn.min.js"};</script>
+<script src="https://miqdadridho.github.io/mycv/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="https://miqdadridho.github.io/mycv/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
+<script src="https://miqdadridho.github.io/mycv/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.14.0" id="regenerator-runtime-js"></script>
+<script src="https://miqdadridho.github.io/mycv/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script src="https://miqdadridho.github.io/mycv/wp-includes/js/dist/hooks.min.js?ver=c6aec9a8d4e5a5d543a1" id="wp-hooks-js"></script>
+<script src="https://miqdadridho.github.io/mycv/wp-includes/js/dist/i18n.min.js?ver=7701b0c3857f914212ef" id="wp-i18n-js"></script>
+<script id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
+</script>
+<script id="password-strength-meter-js-extra">var pwsL10n = {"unknown":"Kekuatan sandi tak diketahui","short":"Sangat lemah","bad":"Lemah","good":"Sedang","strong":"Kuat","mismatch":"Tidak sama"};</script>
+<script id="password-strength-meter-js-translations">( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2024-02-01 12:30:59+0000","generator":"GlotPress\/4.0.0-beta.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"id"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s telah kedaluarsa sejak versi %2$s! Gunakan %3$s sebagai gantinya. Silakan pertimbangkan untuk menulis kode yang lebih inklusif."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );</script>
+<script src="https://miqdadridho.github.io/mycv/wp-admin/js/password-strength-meter.min.js?ver=6.4.3" id="password-strength-meter-js"></script>
+<script src="https://miqdadridho.github.io/mycv/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
+<script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/mycv\/wp-admin\/admin-ajax.php"}};</script>
+<script src="https://miqdadridho.github.io/mycv/wp-includes/js/wp-util.min.js?ver=6.4.3" id="wp-util-js"></script>
+<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"d20d9d6a47"};</script>
+<script id="user-profile-js-translations">( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2024-02-01 12:30:59+0000","generator":"GlotPress\/4.0.0-beta.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n > 1;","lang":"id"},"Your new password has not been saved.":["Sandi baru Anda belum disimpan."],"Hide":["Sembunyikan"],"Show":["Tampilkan"],"Confirm use of weak password":["Setujui penggunaan sandi yang lemah"],"Hide password":["Sembunyikan sandi"],"Show password":["Tampilkan sandi"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );</script>
+<script src="https://miqdadridho.github.io/mycv/wp-admin/js/user-profile.min.js?ver=6.4.3" id="user-profile-js"></script>
+	</body>
+	</html>
